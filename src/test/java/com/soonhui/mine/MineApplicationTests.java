620c9424974e072147c56748825be997368d35ac
soonhuisky <soonhuisky@qq.com>
2018-08-30 09:40:13 +0800

    hhh

--- src/test/java/com/soonhui/mine/MineApplicationTests.java
+++ src/test/java/com/soonhui/mine/MineApplicationTests.java
@@ -14,7 +14,6 @@ public class MineApplicationTests {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
     }
 
 }
